61fcaf0d5d69aac5dcf55e3856d7c90bfad4859f
DrSlowpokePhd <dohertyjames1999@gmail.com>
2020-02-20 00:06:50 -0800

    take 2 lol

--- warm-n-comfy/kitty/.config/kitty/change_color.py
+++
@@ -1,5 +0,0 @@
-def main(args):
-    pass
-
-def handle_result(args, target_window_id, boss):
-    print(boss)
--- warm-n-comfy/kitty/.config/kitty/changecolor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [[ $COLOR_SCHEME == "dark" ]]; then 
-    kitty @ set-colors -a ~/.config/kitty/gruvbox-light.col \ 
-    export COLOR_SCHEME="light"  
-elif [[ $COLOR_SCHEME == "light" ]]; then 
-    kitty @ set-colors -a ~/.config/kitty/gruvbox-dark.col \
-    export COLOR_SCHEME="dark"  
-fi
